5e9fb2c433c953be00610739752aa2c3d48a2470
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-05-01 23:01:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+// get references to the elements
+const status = document.getElementById("status");
+const enterBtn = document.getElementById("enterBtn");
+
+// add a click event listener to the button
+enterBtn.addEventListener("click", function() {
+  // create a new h1 element
+  const h1 = document.createElement("h1");
+  // create a text node with the updated text
+  const text = document.createTextNode("Entered Metaverse");
+  // add the text node to the h1 element
+  h1.appendChild(text);
+  // replace the p tag with the h1 element
+  status.parentNode.replaceChild(h1, status);
+});
